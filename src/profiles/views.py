ad65069a0fc2b7392cab8761e04dc90821e9906f
liangcorp <cliang86@gmail.com>
2020-06-13 01:35:22 +0100

    clean up formats

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 
-
 User = get_user_model()
+
 
 # Create your views here.
 @login_required
